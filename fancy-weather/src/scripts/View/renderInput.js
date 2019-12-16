d345cae9b6868ff50ae7bbc171c93c30df2dd4fd
Aykuli <aykuli@ya.ru>
2019-12-16 22:57:40 +0700

    refactor: redo speech button to be not absolut

--- fancy-weather/src/scripts/View/renderInput.js
+++ fancy-weather/src/scripts/View/renderInput.js
@@ -7,15 +7,15 @@ export default function renderInput(controls, lang) {
   cityInput.setAttribute('placeholder', controlsLocale[lang][1]);
   cityInput.setAttribute('type', 'text');
   cityInput.setAttribute('id', 'city-input');
-
   const cityInputLabel = createElement('label', 'visually-hidden', citySearchForm);
   cityInputLabel.setAttribute('for', 'city-input');
-  const cityBtn = createElement('button', 'controls__search--btn', citySearchForm);
 
-  const speechBg = createElement('span', 'controls__speech--bg pos0', citySearchForm);
   const speechBtn = createElement('button', 'controls__speech--btn', citySearchForm);
-  const speechBtnText = createElement('span', 'visually-hidden', speechBg);
+  const speechBg = createElement('span', 'controls__speech--bg pos0', speechBtn);
+  const microphone = createElement('span', 'controls__speech--mic', speechBtn);
+  const speechBtnText = createElement('span', 'visually-hidden', microphone);
   speechBtnText.innerText = 'Search city by speech';
+  const cityBtn = createElement('button', 'controls__search--btn', citySearchForm);
 
   return [citySearchForm, cityInput, cityBtn, speechBtn, speechBg];
 }
